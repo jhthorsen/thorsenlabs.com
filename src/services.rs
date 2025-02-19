c04eff781080429680b88fd31ed9b67e07780e87
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-02-19 20:32:04 +0900

    No need to import functions in services.rs

--- src/services.rs
+++ src/services.rs
@@ -1,10 +1,6 @@
 use actix_files::Files;
 use actix_web::web;
 
-use arbeidsdager::get_arbeidsdager_table;
-use article::get_article;
-use blog::{get_blog_index, get_blog_post};
-
 mod arbeidsdager;
 mod article;
 mod blog;
@@ -17,37 +13,37 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 
     cfg.service(
         web::resource("/arbeidsdager/table/{year}")
-            .route(web::get().to(get_arbeidsdager_table))
-            .route(web::head().to(get_arbeidsdager_table)),
+            .route(web::get().to(arbeidsdager::get_arbeidsdager_table))
+            .route(web::head().to(arbeidsdager::get_arbeidsdager_table)),
     );
 
     cfg.service(
         web::resource("/blog")
-            .route(web::get().to(get_blog_index))
-            .route(web::head().to(get_blog_index)),
+            .route(web::get().to(blog::get_blog_index))
+            .route(web::head().to(blog::get_blog_index)),
     );
 
     cfg.service(
         web::resource("/blog.html")
-            .route(web::get().to(get_blog_index))
-            .route(web::head().to(get_blog_index)),
+            .route(web::get().to(blog::get_blog_index))
+            .route(web::head().to(blog::get_blog_index)),
     );
 
     cfg.service(
         web::resource("/blog/{blog_id}")
-            .route(web::get().to(get_blog_post))
-            .route(web::head().to(get_blog_post)),
+            .route(web::get().to(blog::get_blog_post))
+            .route(web::head().to(blog::get_blog_post)),
     );
 
     cfg.service(
         web::resource("/blog/{blog_id}.html")
-            .route(web::get().to(get_blog_post))
-            .route(web::head().to(get_blog_post)),
+            .route(web::get().to(blog::get_blog_post))
+            .route(web::head().to(blog::get_blog_post)),
     );
 
     cfg.service(
         web::resource("/{article:.*}")
-            .route(web::get().to(get_article))
-            .route(web::head().to(get_article)),
+            .route(web::get().to(article::get_article))
+            .route(web::head().to(article::get_article)),
     );
 }
